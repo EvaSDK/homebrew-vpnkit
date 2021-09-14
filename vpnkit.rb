9e76d3c36aa20bf5eef84eed53172996f71fa9a2
Gilles Dartiguelongue <gilles.dartiguelongu@rakuten.com>
2021-09-14 14:49:25 +0200

    Apply brew style --fix

--- vpnkit.rb
+++ vpnkit.rb
@@ -1,27 +1,29 @@
+# typed: false
+# frozen_string_literal: true
+
 class Vpnkit < Formula
-  desc "A toolkit for embedding VPN capabilities in your application"
+  desc "Toolkit for embedding VPN capabilities in your application"
   homepage "https://github.com/moby/vpnkit"
   url "https://github.com/moby/vpnkit",
-    :using => :git,
-    :revision => "ea9dbeaf887f5dad8391f4a34d127501fb6bbf64"
+      using:    :git,
+      revision: "ea9dbeaf887f5dad8391f4a34d127501fb6bbf64"
 
   version "ea9dbeaf887f5dad8391f4a34d127501fb6bbf64"
 
   bottle do
     root_url "https://github.com/bensallen/homebrew-vpnkit/releases/download/ea9dbea"
-    cellar :any_skip_relocation
-    sha256 "785015eb38f16927674e24aac7fa3565eec753c145687c918ec45c4a1c8b276b" => :mojave
+    sha256 cellar: :any_skip_relocation, mojave: "785015eb38f16927674e24aac7fa3565eec753c145687c918ec45c4a1c8b276b"
   end
 
-  depends_on :x11 => :build
-  depends_on :xcode => ["9.0", :build]
   depends_on "aspcud" => :build
   depends_on "autoconf" => :build
+  depends_on "https://gist.githubusercontent.com/djs55/7a94ee5aeb882ef5399c0485d2affdda/raw/bc04ff96e0082d7ee07642337dbb77c51b93d678/opam.rb" => :build
   depends_on "ocaml" => :build
   depends_on "pkg-config" => :build
+  depends_on x11: :build
+  depends_on xcode: ["9.0", :build]
   # VPNKit requires an older version of OPAM, use the project's formula for 1.2.2_4.
   # The project is using this as part of their CircleCI.
-  depends_on "https://gist.githubusercontent.com/djs55/7a94ee5aeb882ef5399c0485d2affdda/raw/bc04ff96e0082d7ee07642337dbb77c51b93d678/opam.rb" => :build
 
   def install
     ENV["OPAMYES"] = "1"
@@ -30,7 +32,7 @@ class Vpnkit < Formula
     ENV["OPAM_REPO"] = "repo/darwin"
     ENV["OPAM_COMP"] = "4.07.0"
     ENV["OPAMVERBOSE"] = "1"
-    
+
     system "make"
     File.rename("vpnkit.exe", "vpnkit")
     bin.install "vpnkit"
